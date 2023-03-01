6b44b0912ed6649d93974c1e1b6bbccd20b72ec3
Julien ROGER <julien.roger@net-helium.fr>
2023-03-01 17:07:59 +0100

    ajout compilation assets - pictos jquery-ui

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,3 +3,6 @@ Rails.application.config.assets.precompile += %w( pencil.png cross.png )
 
 # Javascript
 Rails.application.config.assets.precompile += %w( pops_redmine_engine/project_identifier.js )
+
+# Images
+Rails.application.config.assets.precompile += %w( jquery/images/ui-icons_ffffff_256x240.png )
